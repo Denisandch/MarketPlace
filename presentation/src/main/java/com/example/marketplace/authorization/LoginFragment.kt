d79c00a6bb88ea6604983d4c89a2e2f7167a1e25
Denis Samolovov <denissamolovov@gmail.com>
2023-03-08 23:33:19 +0300

    ui 0.1v

---
+++ presentation/src/main/java/com/example/marketplace/authorization/LoginFragment.kt
@@ -0,0 +1,19 @@
+package com.example.marketplace.authorization
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
