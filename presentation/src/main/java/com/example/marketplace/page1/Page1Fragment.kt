c0ab8b44bc15ab4d4b5f97f442366cb88138a8d6
Denis Samolovov <denissamolovov@gmail.com>
2023-03-10 13:41:10 +0300

    ui 0.8v navigation

--- presentation/src/main/java/com/example/marketplace/page1/Page1Fragment.kt
+++ presentation/src/main/java/com/example/marketplace/page1/Page1Fragment.kt
@@ -5,17 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.marketplace.R
+import com.example.marketplace.databinding.FragmentPage1Binding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class Page1Fragment : Fragment() {
 
+    private lateinit var binding: FragmentPage1Binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_page1, container, false)
+        binding = FragmentPage1Binding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.avatarPage1.setOnClickListener {
+            val botNav = activity?.findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            botNav?.selectedItemId = R.id.profile
+        }
+    }
 }
--- presentation/src/main/java/com/example/marketplace/profile/ProfileFragment.kt
+++ presentation/src/main/java/com/example/marketplace/profile/ProfileFragment.kt
@@ -1,21 +1,34 @@
 package com.example.marketplace.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.marketplace.R
+import com.example.marketplace.authorization.AuthorizationActivity
+import com.example.marketplace.databinding.FragmentProfileBinding
 
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonLogOut.setOnClickListener {
+            startActivity(Intent(requireContext(), AuthorizationActivity::class.java))
+            requireActivity().finish()
+        }
     }
 
 }
