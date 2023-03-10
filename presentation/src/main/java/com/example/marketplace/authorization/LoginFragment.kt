efb388f6b128dc98d1e1a0c8ce275bb0eeb34674
Denis Samolovov <denissamolovov@gmail.com>
2023-03-10 13:22:17 +0300

    ui 0.7v

--- presentation/src/main/java/com/example/marketplace/authorization/LoginFragment.kt
+++ presentation/src/main/java/com/example/marketplace/authorization/LoginFragment.kt
@@ -1,19 +1,34 @@
 package com.example.marketplace.authorization
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.marketplace.MainActivity
+import com.example.marketplace.R
+import com.example.marketplace.databinding.FragmentLoginBinding
+import com.example.marketplace.databinding.FragmentSignBinding
 
 
 class LoginFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    private lateinit var binding: FragmentLoginBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonLoginLogin.setOnClickListener {
+            startActivity(Intent(requireContext(), MainActivity::class.java))
+            requireActivity().finish()
+        }
+    }
 
 }
--- presentation/src/main/java/com/example/marketplace/authorization/SignFragment.kt
+++ presentation/src/main/java/com/example/marketplace/authorization/SignFragment.kt
@@ -5,13 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.marketplace.R
+import com.example.marketplace.databinding.FragmentSignBinding
 
 
 class SignFragment : Fragment() {
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding: FragmentSignBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonLogInSign.setOnClickListener {
+            findNavController().navigate(R.id.action_signFragment_to_loginFragment)
+        }
     }
 
 }
